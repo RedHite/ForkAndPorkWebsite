b5f901f2deee18fc2c2ec9c97df1b1fad77f5e31
Wiktor <zwiktor2023032@gmail.com>
2019-10-13 20:22:42 +0200

    CRUDDone

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Meal,Allergene
 
+
+admin.site.register(Allergene)
+admin.site.register(Meal)
 # Register your models here.
--- restaurant/models.py
+++ restaurant/models.py
@@ -18,12 +18,18 @@ VEGEOPTION = (
 class Allergene(models.Model):
     name = models.CharField(max_length=128)
 
+    def __str__(self):
+        return self.name
+
 class Meal(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField(null=True)
     price = models.DecimalField(null=True, max_digits=6, decimal_places=3)
-    calories = models.IntegerField(max_length=5, null=True)
+    calories = models.IntegerField(null=True)
     spicy = models.IntegerField(choices=SPICYPOWER)
     meat = models.IntegerField(choices=VEGEOPTION)
-    picture = models.ImageField()
+    picture = models.ImageField(null=True, blank=True)
     allergenes = models.ManyToManyField(Allergene)
+
+    def __str__(self):
+        return self.name
